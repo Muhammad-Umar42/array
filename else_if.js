8c7e736ad282510c18b759272b1e1060978d7430
mumar <umrking8877@gmail.com>
2022-05-16 20:06:19 +0500

    if_else_work

---
+++ else_if.js
@@ -0,0 +1,10 @@
+let age = 42;
+if (age >= 20 && age < 40) {
+  document.write("you are young boy");
+} else if (age >= 40 && age < 60) {
+  document.write("you are young man");
+} else if (age >= 60 && age < 80) {
+  document.write("you are old man");
+} else {
+  document.write("Entr valid age");
+}
